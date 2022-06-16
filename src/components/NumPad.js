b222ed24ab83ce96812fcec2ed90355dfc8de99e
Juane003 <j.emilio164@gmail.com>
2022-06-16 09:50:12 -0300

    Changed Display and Numpad to functional components

--- src/components/NumPad.js
+++ src/components/NumPad.js
@@ -1,27 +1,22 @@
-import React, { Component } from "react";
+import React from "react";
 import './numpad.css'
 
-class NumPad extends Component{
-    constructor(){
-        super();
-    }
-    render(){
-        const numbersAndOperationsArray = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [0, '+', '-',] ,['=', "CLEAR"]];
-        return(
-            numbersAndOperationsArray.flat().map((btn, i) => {
-                return (    
-                    <div className="box">
-                        <button key={i}
-                            value={btn}
-                            type="button"
-                            onClick={this.props.clicks}>
-                                {btn}
-                            </button>
-                    </div>
-                )
-            })
-        );
-    }
+const NumPad = ({ clicks }) => {
+    const numbersAndOperationsArray = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [0, '+', '-',] ,['=', "CLEAR"]];
+    return(
+        numbersAndOperationsArray.flat().map((btn, i) => {
+            return (    
+                <div className="box">
+                    <button key={i}
+                        value={btn}
+                        type="button"
+                        onClick={clicks}>
+                            {btn}
+                        </button>
+                </div>
+            )
+        })
+    );
 }
 
 export default NumPad;
