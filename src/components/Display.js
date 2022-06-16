d5d73a3328ed222efe1bc47e737ce5615a25b9be
Juane003 <j.emilio164@gmail.com>
2022-06-16 10:18:22 -0300

    fixed identation

--- src/components/Display.js
+++ src/components/Display.js
@@ -2,17 +2,13 @@ import React from "react"
 import './display.css'
 
 const Display = ( {text, operator, result }) => {
-    return(
-        <div className="display">
-            <h2 className="text">Input: {text}</h2>
-                <h2 className="text">{operator}</h2>
-                <h2 className="text">Resultado: {result}</h2>
-            </div>
-    )
+  return(
+    <div className="display">
+      <h2 className="text">Input: {text}</h2>
+			<h2 className="text">{operator}</h2>
+			<h2 className="text">Resultado: {result}</h2>
+		</div>
+  )
 }
-
-
-
-
 
 export default Display;
